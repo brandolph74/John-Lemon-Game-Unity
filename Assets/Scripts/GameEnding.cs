3496442c04eee49f4fe34fa83198e6b591c3b46f
brandolph <42746790+brandolph74@users.noreply.github.com>
2020-09-10 17:43:53 -0600

    Added Camera, Lighting, EndGame
    steps 4-6

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+
+    public float displayImageDuration = 1f;
+
+    public GameObject player;
+
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+
+    float m_Timer;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void Update()
+    {
+        if (m_IsPlayerAtExit)    //remember, this means it is true
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+   
+}
